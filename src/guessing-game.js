1f9d580d4ee88788f1c6891f5f8fce5fd42fe141
Evgeniy <defender35185@gmail.com>
2024-09-05 16:19:10 +0300

    feat: complete guessing game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.middle = Math.ceil((this.min + this.max) / 2);
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle
     }
 
     greater() {
-
+        this.min = this.middle
     }
 }
 
